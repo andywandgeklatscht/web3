9c538dc4d5ce9a558e76741615624c99dc72b752
andywandgeklatscht <100236499+andywandgeklatscht@users.noreply.github.com>
2022-02-24 00:02:38 +0100

    Update html.js

--- html.js
+++ html.js
@@ -3,7 +3,11 @@ const http = require("http");
 const host = '0.0.0.0';
 const port = 8000;
 
-const requestListener = function (req, res) {};
+const requestListener = function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  res.end('<html><body><h1>this is html</h1></body></html>');
+};
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
