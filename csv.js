aba244c3b2171a609593a0cea1dccb6913ea05ee
andywandgeklatscht <100236499+andywandgeklatscht@users.noreply.github.com>
2022-02-24 00:00:47 +0100

    Update csv.js

--- csv.js
+++ csv.js
@@ -6,6 +6,8 @@ const port = 8000;
 const requestListener = function (req, res) {
   res.setHeader("Content-Type", "text/csv");
   res.setHeader("Content-Disposition", "attachment;filename=example.csv")
+  res.writeHead(200);
+  res.end('id,name,email\n1,Sammy Shark,shark@ocean.com');
 };
 
 const server = http.createServer(requestListener);
