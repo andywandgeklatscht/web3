36e553c3e48b927dd2f254d903ba1116a22ca859
andywandgeklatscht <100236499+andywandgeklatscht@users.noreply.github.com>
2022-02-24 03:53:28 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
-const http = require("http");
-
-const host = '0.0.0.0';
-const port = 8000;
-
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end("my server");
-};
-const server = http createServer(requestListener);
-server.listen(port, host, () => {
-  console.log('Server is running!');
-});
+// Modern dapp browsers...
+if (window.ethereum) {
+  App.web3Provider = window.ethereum;
+  try {
+    // Request account access
+    await window.ethereum.request({ method: "eth_requestAccounts" });;
+  } catch (error) {
+    // User denied account access...
+    console.error("User denied account access")
+  }
+}
+// Legacy dapp browsers...
+else if (window.web3) {
+  App.web3Provider = window.web3.currentProvider;
+}
+// If no injected web3 instance is detected, fall back to Ganache
+else {
+  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8000');
+}
+web3 = new Web3(App.web3Provider);
